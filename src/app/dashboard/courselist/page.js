0feb9292d47cd1536b6a577d979de103d5c60b85
BeyonderSS <ssbeyonder@gmail.com>
2023-06-08 12:40:43 +0530

    last commit fix

---
+++ src/app/dashboard/courselist/page.js
@@ -0,0 +1,69 @@
+"use client";
+
+import React, { Suspense, useEffect, useState } from "react";
+import CourseCard from "../CourseCard";
+import { useSession } from "next-auth/react";
+import listCourses from "@/lib/listCourses";
+import CourseCardSkeleton from "../CourseCardSkeleton";
+
+const Courses = () => {
+  const [course, setCourse] = useState([]);
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (session) {
+      setLoading(true);
+      console.log(session.accessToken);
+
+      async function getCourses(access_token) {
+        const accessToken = access_token;
+        const courses = await listCourses(accessToken);
+        console.log(courses);
+        setCourse(courses);
+        setLoading(false);
+      }
+
+      getCourses(session.accessToken);
+    }
+  }, [session]);
+  const role = "Teacher";
+  if (loading) {
+    return (
+      <div className="lg:pl-80 pt-20">
+        <CourseCardSkeleton />
+      </div>
+    );
+  }
+  if (course.length == 0) {
+    return (
+      <div className="lg:pl-80 pt-20 h-screen bg-blue-200">
+        <div className="flex justify-center items-center lg:text-5xl text-4xl text-white/90 font-semibold m-4  ">
+          <h1 className=" p-3 px-6 rounded-lg bg-blue-400 flex justify-center items-center">
+            Your Courses
+          </h1>
+        </div>
+        Opps it Looks Like you are not enrolled in any courses!
+      </div>
+    );
+  }
+  return (
+    <div className="lg:pl-80 pt-20 overflow-x-hidden bg-blue-200 py-6 h-screen">
+      <div className="mx-10">
+        <div className="flex justify-center items-center lg:text-5xl text-4xl text-white/90 font-semibold m-4  ">
+          <h1 className=" p-3 px-6 rounded-lg bg-blue-400 flex justify-center items-center">
+            Your Courses
+          </h1>
+        </div>
+        <Suspense
+          fallback={
+            <p className="flex justify-center items-center">loading....</p>
+          }
+        >
+          <CourseCard courses={course} role={role} />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default Courses;
