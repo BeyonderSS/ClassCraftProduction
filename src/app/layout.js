6f6d46d61f954155649601bed3fe5373b5820b3b
BeyonderSS <ssbeyonder@gmail.com>
2023-06-14 20:09:48 +0530

    Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-// app/layout.js 
+// app/layout.js
 import ProvidersWrapper from "./ProvidersWrapper";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
       <body>
         <ProvidersWrapper>
           {children}
+          <Analytics />
         </ProvidersWrapper>
       </body>
     </html>
