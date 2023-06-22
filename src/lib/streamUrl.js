e8947488879624660cb5dd1160b0b9995aa6568f
spideyo0 <dshantanu2003@gmail.com>
2023-06-22 21:58:41 +0530

    updated default quality settings

--- src/lib/streamUrl.js
+++ src/lib/streamUrl.js
@@ -4,7 +4,7 @@ import ytdl from 'ytdl-core';
 export async function getYoutubeVideoUrl(url) {
   try {
     const info = await ytdl.getInfo(url);
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
     return format.url;
   } catch (error) {
     throw error;
