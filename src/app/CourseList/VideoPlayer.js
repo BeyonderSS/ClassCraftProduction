fbdf60dd1411d968e3195a220207b3cec7b741a4
spideyo0 <dshantanu2003@gmail.com>
2023-07-31 12:08:45 +0530

    switched to default video player

--- src/app/CourseList/VideoPlayer.js
+++ src/app/CourseList/VideoPlayer.js
@@ -20,8 +20,7 @@ const VideoPlayer = ({ videoUrl }) => {
   
 
   return (
-    <video controls>
-      {streamUrl && <source src={streamUrl} type="video/mp4" />}
+    <video src={'https://rr2---sn-ci5gup-civz.googlevideo.com/videoplayback?expire=1690806654&ei=HlXHZJ3THPDFg8UP8NCwCA&ip=171.60.170.25&id=o-APSDyqXAe8GMQuT-i-pnkxaQnGofK-JQMnc6RO7pcm9n&itag=22&source=youtube&requiressl=yes&mh=Ih&mm=31%2C29&mn=sn-ci5gup-civz%2Csn-ci5gup-h55r&ms=au%2Crdu&mv=m&mvi=2&pcm2cms=yes&pl=23&initcwndbps=1248750&spc=Ul2SqyFVzkWFNoCpF3jSIkiv4IQzaYlBroZgNkks8A&vprv=1&svpuc=1&mime=video%2Fmp4&ns=eI3Ykm7TLgD4fv21qz3xN1cO&cnr=14&ratebypass=yes&dur=1071.763&lmt=1690622404203303&mt=1690784718&fvip=2&fexp=24007246%2C24363391%2C51000011&beids=24350018&c=WEB&txp=5532434&n=zuX-vsqvFvxfqw&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhAMejskxdvVy_7Ue1yj920EJf2qWeqLxbbKsFeAjS8W4-AiB9XXsqgzsiyARiyZctQAakcaSmzM7IgfeDLDGCqeeY8A%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgO3C7hi5HGzW695x-KLgi1o0LB24smlgSxjt4lJNYi0UCIQDLk5eFysVGhbrShmaxgnBhVWh9EitcX-EGbolwJ_3-wA%3D%3D'} controls controlsList="nodownload noplaybackrate" disablePictureInPicture>
       Your browser does not support the video tag.
     </video>
   );
--- src/app/dashboard/CustomVideoPlayer.js
+++ src/app/dashboard/CustomVideoPlayer.js
@@ -34,209 +34,212 @@ const CustomVideoPlayer = ({ videoLink }) => {
     fetchStreamUrl();
   }, [videoLink]);
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.controls = false;
-    }
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     videoRef.current.controls = false;
+  //   }
 
-    document.addEventListener("fullscreenchange", handleFullScreenChange);
-    document.addEventListener("keydown", handleKeyPress);
+  //   document.addEventListener("fullscreenchange", handleFullScreenChange);
+  //   document.addEventListener("keydown", handleKeyPress);
 
-    // Add event listeners to prevent right-clicking, screen shots, and video download
-    document.addEventListener("contextmenu", handleContextMenu);
+  //   // Add event listeners to prevent right-clicking, screen shots, and video download
+  //   document.addEventListener("contextmenu", handleContextMenu);
 
-    return () => {
-      document.removeEventListener("fullscreenchange", handleFullScreenChange);
-      document.removeEventListener("keydown", handleKeyPress);
-      document.removeEventListener("contextmenu", handleContextMenu);
-    };
-  }, [videoRef.current]);
+  //   return () => {
+  //     document.removeEventListener("fullscreenchange", handleFullScreenChange);
+  //     document.removeEventListener("keydown", handleKeyPress);
+  //     document.removeEventListener("contextmenu", handleContextMenu);
+  //   };
+  // }, [videoRef.current]);
 
-  useEffect(() => {
-    const handleTimeUpdate = () => {
-      // Check if the videoRef has been set and is not null
-      if (videoRef.current) {
-        const currentTime = videoRef.current.currentTime;
-        const duration = videoRef.current.duration;
-        const videoProgress = (currentTime / duration) * 100;
-        setProgress(videoProgress);
-      }
-    };
+  // useEffect(() => {
+  //   const handleTimeUpdate = () => {
+  //     // Check if the videoRef has been set and is not null
+  //     if (videoRef.current) {
+  //       const currentTime = videoRef.current.currentTime;
+  //       const duration = videoRef.current.duration;
+  //       const videoProgress = (currentTime / duration) * 100;
+  //       setProgress(videoProgress);
+  //     }
+  //   };
 
-    // Check if the videoRef has been set and is not null
-    if (videoRef.current) {
-      videoRef.current.addEventListener("timeupdate", handleTimeUpdate);
-    }
+  //   // Check if the videoRef has been set and is not null
+  //   if (videoRef.current) {
+  //     videoRef.current.addEventListener("timeupdate", handleTimeUpdate);
+  //   }
 
-    return () => {
-      // Check if the videoRef has been set and is not null
-      if (videoRef.current) {
-        videoRef.current.removeEventListener("timeupdate", handleTimeUpdate);
-      }
-    };
-  }, [videoRef.current]);
+  //   return () => {
+  //     // Check if the videoRef has been set and is not null
+  //     if (videoRef.current) {
+  //       videoRef.current.removeEventListener("timeupdate", handleTimeUpdate);
+  //     }
+  //   };
+  // }, [videoRef.current]);
 
-  const handleFullScreenChange = () => {
-    setIsFullScreen(!!document.fullscreenElement);
-  };
+  // const handleFullScreenChange = () => {
+  //   setIsFullScreen(!!document.fullscreenElement);
+  // };
 
-  const handlePlayPause = () => {
-    if (videoRef.current && videoRef.current.paused) {
-      videoRef.current.play();
-      setIsPlaying(true);
-    } else {
-      videoRef.current.pause();
-      setIsPlaying(false);
-    }
-  };
+  // const handlePlayPause = () => {
+  //   if (videoRef.current && videoRef.current.paused) {
+  //     videoRef.current.play();
+  //     setIsPlaying(true);
+  //   } else {
+  //     videoRef.current.pause();
+  //     setIsPlaying(false);
+  //   }
+  // };
 
-  const handleSkipBackward = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime -= 10;
-    }
-  };
+  // const handleSkipBackward = () => {
+  //   if (videoRef.current) {
+  //     videoRef.current.currentTime -= 10;
+  //   }
+  // };
 
-  const handleSkipForward = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime += 10;
-    }
-  };
+  // const handleSkipForward = () => {
+  //   if (videoRef.current) {
+  //     videoRef.current.currentTime += 10;
+  //   }
+  // };
 
-  const handleVolumeChange = (e) => {
-    const newVolume = parseFloat(e.target.value);
-    setVolume(newVolume);
-    if (videoRef.current) {
-      videoRef.current.volume = newVolume;
-    }
-  };
+  // const handleVolumeChange = (e) => {
+  //   const newVolume = parseFloat(e.target.value);
+  //   setVolume(newVolume);
+  //   if (videoRef.current) {
+  //     videoRef.current.volume = newVolume;
+  //   }
+  // };
 
-  const handleMouseMove = () => {
-    if (streamUrl) {
-      controlsAnimation.start({ opacity: 1 });
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        controlsAnimation.start({ opacity: 0 });
-      }, 2000);
-    }
-  };
+  // const handleMouseMove = () => {
+  //   if (streamUrl) {
+  //     controlsAnimation.start({ opacity: 1 });
+  //     clearTimeout(timeoutId);
+  //     timeoutId = setTimeout(() => {
+  //       controlsAnimation.start({ opacity: 0 });
+  //     }, 2000);
+  //   }
+  // };
 
-  const handleKeyPress = (e) => {
-    if (e.code === "Space") {
-      handlePlayPause();
-    }
-    if (e.code === "ArrowRight") {
-      handleSkipForward();
-    } else if (e.code === "ArrowLeft") {
-      handleSkipBackward();
-    }
-  };
+  // const handleKeyPress = (e) => {
+  //   if (e.code === "Space") {
+  //     handlePlayPause();
+  //   }
+  //   if (e.code === "ArrowRight") {
+  //     handleSkipForward();
+  //   } else if (e.code === "ArrowLeft") {
+  //     handleSkipBackward();
+  //   }
+  // };
 
-  const handleContextMenu = (e) => {
-    e.preventDefault();
-  };
+  // const handleContextMenu = (e) => {
+  //   e.preventDefault();
+  // };
 
-  const handleResetVideo = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime = 0;
-    }
-  };
+  // const handleResetVideo = () => {
+  //   if (videoRef.current) {
+  //     videoRef.current.currentTime = 0;
+  //   }
+  // };
 
-  // Utility function to format time in MM:SS format
-  const formatTime = (timeInSeconds) => {
-    const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = Math.floor(timeInSeconds % 60);
-    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
-      2,
-      "0"
-    )}`;
-  };
+  // // Utility function to format time in MM:SS format
+  // const formatTime = (timeInSeconds) => {
+  //   const minutes = Math.floor(timeInSeconds / 60);
+  //   const seconds = Math.floor(timeInSeconds % 60);
+  //   return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+  //     2,
+  //     "0"
+  //   )}`;
+  // };
 
   return (
     streamUrl && (
-      <div
-        className="relative md:h-[90vh] h-96  aspect-w-16 aspect-h-9 bg-black"
-        onMouseMove={handleMouseMove}
-        onMouseLeave={() => {
-          controlsAnimation.start({ opacity: 0 });
-          clearTimeout(timeoutId);
-        }}
-      >
-        <video
-          ref={videoRef}
-          src={streamUrl}
-          className="absolute inset-0 w-full h-full object-cover"
-          loop
-          controls={false}
-          volume={volume}
-          onPlay={() => setIsPlaying(true)}
-          onPause={() => setIsPlaying(false)}
-          onVolumeChange={(e) => setVolume(e.target.volume)}
-          onEnded={() => setIsPlaying(false)}
-        />
+      // <div
+      //   className="relative md:h-[90vh] h-96  aspect-w-16 aspect-h-9 bg-black"
+      //   onMouseMove={handleMouseMove}
+      //   onMouseLeave={() => {
+      //     controlsAnimation.start({ opacity: 0 });
+      //     clearTimeout(timeoutId);
+      //   }}
+      // >
+      //   <video
+      //     ref={videoRef}
+      //     src={streamUrl}
+      //     className="absolute inset-0 w-full h-full object-cover"
+      //     loop
+      //     controls={false}
+      //     volume={volume}
+      //     onPlay={() => setIsPlaying(true)}
+      //     onPause={() => setIsPlaying(false)}
+      //     onVolumeChange={(e) => setVolume(e.target.volume)}
+      //     onEnded={() => setIsPlaying(false)}
+      //   />
 
-        <motion.div
-          ref={controlsRef}
-          className={`absolute bottom-10 left-1/2 transform -translate-x-1/2 flex items-center justify-center bg-black bg-opacity-70 rounded-md p-4 ${
-            isFullScreen || controlsAnimation.opacity > 0
-              ? "opacity-100"
-              : "opacity-0"
-          }`}
-          initial={{ opacity: 0 }}
-          animate={controlsAnimation}
-        >
-          <button onClick={handleSkipBackward} className="text-blue-500 p-2">
-            <BiLeftArrow size={20} />
-          </button>
+      //   <motion.div
+      //     ref={controlsRef}
+      //     className={`absolute bottom-10 left-1/2 transform -translate-x-1/2 flex items-center justify-center bg-black bg-opacity-70 rounded-md p-4 ${
+      //       isFullScreen || controlsAnimation.opacity > 0
+      //         ? "opacity-100"
+      //         : "opacity-0"
+      //     }`}
+      //     initial={{ opacity: 0 }}
+      //     animate={controlsAnimation}
+      //   >
+      //     <button onClick={handleSkipBackward} className="text-blue-500 p-2">
+      //       <BiLeftArrow size={20} />
+      //     </button>
 
-          <button onClick={handlePlayPause} className="text-blue-500 p-2">
-            {isPlaying ? <BsPauseFill size={20} /> : <BsPlayFill size={20} />}
-          </button>
+      //     <button onClick={handlePlayPause} className="text-blue-500 p-2">
+      //       {isPlaying ? <BsPauseFill size={20} /> : <BsPlayFill size={20} />}
+      //     </button>
 
-          <button onClick={handleSkipForward} className="text-blue-500 p-2">
-            <BiRightArrow size={20} />
-          </button>
+      //     <button onClick={handleSkipForward} className="text-blue-500 p-2">
+      //       <BiRightArrow size={20} />
+      //     </button>
 
-          {/* volume section  */}
-          <div className="mx-2 flex justify-center items-center">
-            {volume > 0 ? (
-              <button
-                onClick={() => setVolume(0)}
-                className="text-blue-500 p-2"
-              >
-                <BsVolumeUpFill size={20} />
-              </button>
-            ) : (
-              <button
-                onClick={() => setVolume(0.7)}
-                className="text-blue-500 p-2"
-              >
-                <BsVolumeMuteFill size={20} />
-              </button>
-            )}
-            <input
-              type="range"
-              min={0}
-              max={1}
-              step={0.01}
-              value={volume}
-              onChange={handleVolumeChange}
-              className="w-16 h-2 bg-blue-500"
-            />
-          </div>
-        </motion.div>
-        <div className="absolute bottom-0 left-0 right-0">
-          {/* Display current time and duration */}
-          <div className=" p-2 text-gray-600 font-semibold text-sm flex justify-center items-center">
-            {formatTime(videoRef.current ? videoRef.current.currentTime : 0)} /{" "}
-            {formatTime(videoRef.current ? videoRef.current.duration : 0)}
-          </div>
-          {/* Video progress bar */}
-          <motion.div
-            className="h-2 bg-blue-600"
-            initial={{ scaleX: 0 }}
-            animate={{ scaleX: progress / 100 }}
-          />
-        </div>
+      //     {/* volume section  */}
+      //     <div className="mx-2 flex justify-center items-center">
+      //       {volume > 0 ? (
+      //         <button
+      //           onClick={() => setVolume(0)}
+      //           className="text-blue-500 p-2"
+      //         >
+      //           <BsVolumeUpFill size={20} />
+      //         </button>
+      //       ) : (
+      //         <button
+      //           onClick={() => setVolume(0.7)}
+      //           className="text-blue-500 p-2"
+      //         >
+      //           <BsVolumeMuteFill size={20} />
+      //         </button>
+      //       )}
+      //       <input
+      //         type="range"
+      //         min={0}
+      //         max={1}
+      //         step={0.01}
+      //         value={volume}
+      //         onChange={handleVolumeChange}
+      //         className="w-16 h-2 bg-blue-500"
+      //       />
+      //     </div>
+      //   </motion.div>
+      //   <div className="absolute bottom-0 left-0 right-0">
+      //     {/* Display current time and duration */}
+      //     <div className=" p-2 text-gray-600 font-semibold text-sm flex justify-center items-center">
+      //       {formatTime(videoRef.current ? videoRef.current.currentTime : 0)} /{" "}
+      //       {formatTime(videoRef.current ? videoRef.current.duration : 0)}
+      //     </div>
+      //     {/* Video progress bar */}
+      //     <motion.div
+      //       className="h-2 bg-blue-600"
+      //       initial={{ scaleX: 0 }}
+      //       animate={{ scaleX: progress / 100 }}
+      //     />
+      //   </div>
+      // </div>
+      <div>
+        <video src={streamUrl} controls controlsList="nodownload noplaybackrate" disablePictureInPicture></video>
       </div>
     )
   );
