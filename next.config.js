9d52a3968a39815bacaab011b41ba06d7fd4d435
spideyo0 <dshantanu2003@gmail.com>
2023-10-08 18:37:41 +0530

    right click disable and pwa implemented

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
 const nextConfig = {
+  ...withPWA({
+    dest:'public',
+    register:true,
+    skipwating:true,
+  }),
   experimental: {
     serverActions: true,
   },
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,36 @@
-// app/layout.js
+"use client";
+import { useEffect } from "react";
 import ProvidersWrapper from "./ProvidersWrapper";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
+  // Function to disable right-click
+  const disableRightClick = (e) => {
+    e.preventDefault();
+  };
+
+  // Add a right-click event listener to the body element
+  document.body.addEventListener("contextmenu", disableRightClick);
+
+  // Remove the event listener and restore inspect mode when the component unmounts
+  useEffect(() => {
+    return () => {
+      document.body.removeEventListener("contextmenu", disableRightClick);
+      document.onkeydown = null;
+    };
+  }, []);
+
   return (
     <html lang="en">
-     <head>
+      <head>
         <title>Class Craft || Powered By Flourishers Edge</title>
         <link rel="icon" href="/CC.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
         <ProvidersWrapper>
