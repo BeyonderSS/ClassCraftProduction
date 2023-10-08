2b8dcf387df879264663e2303074b792c0243b51
spideyo0 <dshantanu2003@gmail.com>
2023-10-08 18:55:32 +0530

    disableing some keys

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -6,19 +6,47 @@ import SideBar from "./SideBar";
 
 export default function RootLayout({ children }) {
 
-   // Function to disable right-click
-   const disableRightClick = (e) => {
-    e.preventDefault();
-  };
+  //  // Function to disable right-click
+  //  const disableRightClick = (e) => {
+  //   e.preventDefault();
+  // };
 
-  // Add a right-click event listener to the body element
-  document.body.addEventListener("contextmenu", disableRightClick);
+  // // Add a right-click event listener to the body element
+  // // document.body.addEventListener("contextmenu", disableRightClick);
 
-  // Remove the event listener and restore inspect mode when the component unmounts
+  // // Remove the event listener and restore inspect mode when the component unmounts
+  // useEffect(() => {
+  //   return () => {
+  //     document.body.removeEventListener("contextmenu", disableRightClick);
+  //     document.onkeydown = null;
+  //   };
+  // }, []);
   useEffect(() => {
-    return () => {
-      document.body.removeEventListener("contextmenu", disableRightClick);
-      document.onkeydown = null;
+    typeof window !== undefined &&
+      window.document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+      });
+
+    document.onkeydown = function (e) {
+      console.log(e.key);
+      if (e.key === "F12") {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === "I") {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === "C") {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === "J") {
+        return false;
+      }
+      if (e.ctrlKey && e.key === "u") {
+        return false;
+      }
+      if (e.ctrlKey && e.key === "c") {
+        return false;
+      }
     };
   }, []);
   return (
